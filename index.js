139ba32f2693d15265de780153f801804f176464
neharajesh <leehanseok3@gmail.com>
2021-07-02 06:37:21 +0530

    FEAT: User Management

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ app.use(bodyParser.json());
 app.use(cors())
 const PORT = process.env.PORT;
 
+const authRouter = require("./routers/auth-router")
+const userRouter = require("./routers/user-router")
+const { verifyToken } = require("./middlewares/verify-signin")
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'POST, OPTIONS',  "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+app.use("/auth", authRouter)
+app.use("/user", userRouter)
+
 app.get("/", (req, res) => {
     res.json({success: true, message: "Loaded successfully"})
 })
